04ead16e5476309fbb6a513cdcd282878bf80e41
Sebas Amaya <sebasamayaxd@hotmail.com>
2022-10-03 11:21:42 -0500

    team controller ok

---
+++ controllers/teamsController.js
@@ -0,0 +1,96 @@
+const Team = require('./../models/Team')
+
+const getTeams = async(req, res) => {
+    try {
+        const team = await Team.find()
+        return res.status(200).json({
+            ok: true,
+            team
+        })
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'Please contact to support',
+            error: error
+        })
+    }
+}
+
+const createTeam = async(req, res) => {
+    const { name } = req.body
+    try {
+        const team = await Team.findOne({ name: name })
+        if(team) return res.status(400).json({
+            ok: false,
+            msg: 'This team is already exist!!'
+        })
+        const dbteam = new Team({
+            name: name
+        })
+        dbteam.save()
+        return res.status(200).json({
+            ok: true,
+            msg: 'Team created!'
+        })
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Please contact to support',
+            error: error
+        })
+    }
+}
+
+const deleteTeam = async(req, res) => {
+    const id = req.params.id    
+    try {
+        const team = await Team.findOneAndDelete({ _id: id })
+        if(team === null) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'This ID not exists in database'
+            })
+        }
+        return res.status(200).json({
+            ok: true,
+            msg: 'Team deleted!!'
+        })
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'Invalid ID please try again',
+            error: error
+        })
+    }
+}
+
+const updateTeam = async(req, res) => {
+    const id = req.params.id
+    const { name } = req.body
+    try {
+        const team = await Team.findByIdAndUpdate(id, { name: name })
+        if(team === null) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'ID doesnt exist in database'
+            })
+        }
+        return res.status(200).json({
+            ok: true,
+            msg: 'Team updated!'
+        })
+    } catch (error) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'ID invalid!',
+            error: error
+        })
+    }
+}
+
+module.exports = {
+    getTeams,
+    createTeam,
+    deleteTeam,
+    updateTeam
+}
--- routes/team.js
+++ routes/team.js
@@ -1,4 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const { validateFields } = require('./../middlewares/validationResult')
+const { teams } = require('./../middlewares/validationBody')
+const { getTeams, createTeam, updateTeam, deleteTeam } = require('./../controllers/teamsController')
+
+router.get('/', getTeams)
+
+router.post('/', teams, validateFields, createTeam)
+
+router.delete('/delete/:id', deleteTeam)
+
+router.put('/update/:id', teams, validateFields, updateTeam)
 
 module.exports = router
